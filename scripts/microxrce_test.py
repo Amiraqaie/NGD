2af98f7263f2821f1105d26bee7cfffb8feb8f18
Amiraqaie <amiraqaie1376@gmail.com>
2025-10-19 11:22:53 +0330

    added needed params to airframe and update microxrce python code

--- scripts/microxrce_test.py
+++ scripts/microxrce_test.py
@@ -69,7 +69,7 @@ class OffboardControll(Node):
 
         # Create Subscribtions nodes
         self.subscriber_vehicle_status = self.create_subscription(VehicleStatus,
-                                                                  '/fmu/out/vehicle_status',
+                                                                  '/fmu/out/vehicle_status_v1',
                                                                   self.vehicle_status_callback,
                                                                   qos_profile1)
         
